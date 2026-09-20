655083e4d262475362345769c625c4024fe689e7
Theo <theomatteo.sa@gmail.com>
2026-09-20 10:33:02 -0300

    feat: add Quote class to represent stock quotes

--- src/main/java/com/soe/Main.java
+++ src/main/java/com/soe/Main.java
@@ -1,15 +1,14 @@
 package com.soe;
 
 import com.soe.client.BrapiClient;
+import com.soe.domain.Quote;
 import com.soe.dto.BrapiResponseDTO;
-import com.soe.dto.QuoteDTO;
 
 public class Main {
     public static void main(String[] args) {
         BrapiClient client = new BrapiClient();
         BrapiResponseDTO response = client.getQuote("B3SA3");
-        QuoteDTO quote = response.results().get(0);
-        System.out.println(quote.symbol());
-        System.out.println(quote.regularMarketPrice());
+        Quote quote = new Quote(response.results().get(0));
+        System.out.println(quote);
     }
 }
---
+++ src/main/java/com/soe/domain/Quote.java
@@ -0,0 +1,61 @@
+package com.soe.domain;
+
+import java.time.Instant;
+import com.soe.dto.QuoteDTO;
+import lombok.Data;
+
+@Data
+public class Quote {
+
+    // Descriptive fields for the stock quote
+    String symbol;
+    String name;
+    String logourl;
+    String currency;
+
+    // Timestamp of the last market update
+    Instant regularMarketTime;
+
+    // Number of shares traded today
+    Long regularMarketVolume;
+
+    // Price change information relative to the previous close
+    Double regularMarketChange;
+    Double regularMarketChangePercent;
+
+    // Current market price
+    Double regularMarketPrice;
+
+    // Daily price minimum and maximum values
+    Double regularMarketDayHigh;
+    Double regularMarketDayLow;
+
+    // Previous closing price
+    Double regularMarketPreviousClose;
+
+    // Opening price of the current trading day
+    Double regularMarketOpen;
+
+    // Maximum and minimum prices over the last 52 weeks
+    Double fiftyTwoWeekLow;
+    Double fiftyTwoWeekHigh;
+
+    public Quote(QuoteDTO quoteDTO) {
+        this.symbol = quoteDTO.symbol();
+        this.name = quoteDTO.longName();
+        this.logourl = quoteDTO.logourl();
+        this.currency = quoteDTO.currency();
+        this.regularMarketTime = Instant.parse(quoteDTO.regularMarketTime());
+        this.regularMarketVolume = quoteDTO.regularMarketVolume();
+        this.regularMarketChange = quoteDTO.regularMarketChange();
+        this.regularMarketChangePercent = quoteDTO.regularMarketChangePercent();
+        this.regularMarketPrice = quoteDTO.regularMarketPrice();
+        this.regularMarketDayHigh = quoteDTO.regularMarketDayHigh();
+        this.regularMarketDayLow = quoteDTO.regularMarketDayLow();
+        this.regularMarketPreviousClose = quoteDTO.regularMarketPreviousClose();
+        this.regularMarketOpen = quoteDTO.regularMarketOpen();
+        this.fiftyTwoWeekLow = quoteDTO.fiftyTwoWeekLow();
+        this.fiftyTwoWeekHigh = quoteDTO.fiftyTwoWeekHigh();
+    }
+
+}
